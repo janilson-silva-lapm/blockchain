e2537dec84b9a64e181cbfdd7653c5fe271df566
Janilson Silva <janilson.silva@lapm.com.br>
2024-03-12 12:09:21 -0300

    [ADD]
    Sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -20,7 +20,7 @@ contract simplestorage {
       return storedData;
    }
 
-   function get() public view returns (bytes32 retVal) {
+   function get() public view returns (string retVal) {
       return storedData;
    }
 
