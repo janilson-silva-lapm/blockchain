0dfad4430f4d6abef7d0a0297a61c6000d2253e3
Janilson Silva <janilson.silva@lapm.com.br>
2024-03-12 12:17:53 -0300

    [ADD]
    Sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -3,28 +3,20 @@ pragma solidity ^0.5.0;
 contract simplestorage {
    string public storedData;
 
-   event DataStored (
-      string data
-   );
-
-   constructor(uint initVal, string memory nucontrato) public {
+   constructor(uint initVal, string nucontrato) public {
       storedData = initVal + nucontrato;
    }
 
-   function set(string memory x) public returns (string value) {
+   function set(string x) public {
       require(x < 100, "Value can not be over 100");
       storedData = x;
+   }
 
-      emit DataStored(x);
-
+   function get() public view returns (string) {
       return storedData;
    }
 
-   function get() public view returns (string retVal) {
-      return storedData;
-   }
-
-   function query() public view returns (string retVal) {
+   function query() public view returns (string) {
       return storedData;
    }
 }
