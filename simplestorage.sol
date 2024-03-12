e4cb994d59a938257fb5ed5f597d7f6460ced9e1
Janilson Silva <janilson.silva@lapm.com.br>
2024-03-12 13:39:20 -0300

    [ADD]
    Sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -1,13 +1,17 @@
 pragma solidity ^0.8.2;
 
 contract ContratoStorage {
-
+    
     struct Contrato {
         string nucontrato;
         string data;
     }
 
     mapping(string => Contrato) public contratos;
+
+    constructor(string memory nucontrato, string memory data) {
+        contratos[nucontrato] = Contrato(nucontrato, data);
+    }
 
     function post(string memory nucontrato, string memory data) public {
         contratos[nucontrato] = Contrato(nucontrato, data);
